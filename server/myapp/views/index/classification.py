2e9bd8085cfcb5f22af916c304e68ff8460377d6
2002051 <1257091748@qq.com>
2024-09-12 23:13:48 +0800

    熟悉api。

--- server/myapp/views/index/classification.py
+++ server/myapp/views/index/classification.py
@@ -6,14 +6,24 @@ from myapp.handler import APIResponse
 from myapp.models import Classification
 from myapp.serializers import ClassificationSerializer
 from myapp.utils import dict_fetchall
+from rest_framework.views import APIView
 
-
-@api_view(['GET'])
-def list_api(request):
-    if request.method == 'GET':
+# cbv写法
+class list_api(APIView):
+    def get(self,request):
+        print(123)
         classifications = Classification.objects.all().order_by('-create_time')
         serializer = ClassificationSerializer(classifications, many=True)
         return APIResponse(code=0, msg='查询成功', data=serializer.data)
+
+
+# @api_view(['GET'])
+# def list_api(request):
+#     if request.method == 'GET':
+#         print(123)
+#         classifications = Classification.objects.all().order_by('-create_time')
+#         serializer = ClassificationSerializer(classifications, many=True)
+#         return APIResponse(code=0, msg='查询成功', data=serializer.data)
 
 
 
